0385d2c13dde3c626323f1c4fb646722b1ea19f5
RehIreh <kadek.heriawan511@gmail.com>
2026-03-14 23:41:02 +0800

    update migrations table schema

--- migrations/migrationsTable.js
+++ migrations/migrationsTable.js
@@ -12,7 +12,18 @@ const up = async () => {
           password VARCHAR(255) NOT NULL,
           role VARCHAR(50) NOT NULL
         )        
-        `)
+        `);
+        await pool.query(
+          `CREATE TABLE IF NOT EXISTS userprofile (
+          id INT AUTO_INCREMENT PRIMARY KEY,
+          uuid VARCHAR(100) NOT NULL UNIQUE,
+          address VARCHAR(250) NOT NULL,
+          birthDate DATE NOT NULL,
+          jobs VARCHAR(100) NOT NULL,
+          gender ENUM('L','P','N'),
+          FOREIGN KEY (uuid) REFERENCES users(uuid )
+          )`
+        );
     } catch (err) {
       console.log("Migrasi gagal :", err)
     } finally {
