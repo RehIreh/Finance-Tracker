367e688a44b797898589feaca23cfd7940d036c3
RehIreh <kadek.heriawan511@gmail.com>
2026-03-16 21:03:36 +0800

    feat: add update profile logic

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,8 +4,6 @@ import { pool } from "../config/db.js";
 
 const profile = async(req, res) => {
 
-  console.log(req.user.uuid)
-
   const [[userProfile]] = await pool.query(
     `select u.name, up.address, up.birthDate, up.jobs,up.gender,u.role from userprofile up
     inner join users u on u.uuid = up.uuid
@@ -20,5 +18,43 @@ const profile = async(req, res) => {
 
 };
 
+const updateProfile = async(req, res) => {
 
-export { profile };
+  const updates = req.body;
+
+  const fields = Object.keys(updates);
+  const values = Object.values(updates);
+
+  const setClause = fields.map(field => `${field} = ?`).join(", ");
+
+  try{ 
+  // Verify user if exist
+  const [[userData]] = await pool.query(
+    `select uuid,email from users where uuid = ?`,
+    [req.user.uuid]
+  )
+
+  if(!userData){ 
+    return res.status(400).json({
+      status: "failed",
+      message: "Data tidak ditemukan"
+    })
+  }
+
+  await pool.query(
+    `update userprofile set ${setClause} where uuid = ?`,
+    [...values, req.user.uuid]
+  )
+
+  res.status(200).json({
+    message: "sucess update data",
+    updatedFields : updates
+  })
+  } catch(err) {
+    next(err)
+  }
+
+};
+
+
+export { profile, updateProfile };
